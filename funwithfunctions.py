16714943c29206756d28425e6b78b2f72dfba470
Ryan Armes <ryancarmes@gmail.com>
2018-03-10 14:09:57 -0500

    first commit

--- funwithfunctions.py
+++ funwithfunctions.py
@@ -1,5 +1,5 @@
-def odd_even():
-    for count in range(1, 2001):
+def odd_even(a, b):
+    for count in range(a, b):
         if count % 2 == 0:
             even = "Number is "
             even += str(count)
@@ -9,4 +9,28 @@ def odd_even():
             odd += str(count)
             print odd + ". " + "This is an odd number."
 
-odd_even()
+odd_even(1, 100)
+
+def multiply(arr, num):
+    for count in range(0, len(arr)):
+      arr[count] *= num
+    return arr
+
+list = [2, 4, 10, 16]
+print multiply(list, 5)
+
+def layered_multiples(arr):
+    new_array = []
+    for x in arr:
+        value_arr =  []        
+        for i in range(0,x):
+            value_arr.append(1)
+        new_array.append(value_arr)
+    return new_array
+
+x = layered_multiples(multiply([2,4,5],3))
+print x
+
+
+
+
